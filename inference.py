199c04f7796313c4c6b85b82f337791d14c34a03
Tyler Hughes <thughes@integralads.com>
2023-02-14 13:09:39 +0000

    ML-1168. Changes: M  inference.py

--- inference.py
+++ inference.py
@@ -1,70 +1,71 @@
 import glob
 import os
 import librosa
+
 from model import AudioCLIP
 from utils.transforms import ToTensor1D
 
 import numpy as np
 import torch
 
-def load_aclp(model_filename = 'AudioCLIP-Full-Training.pt'):
-    aclp = AudioCLIP(pretrained=f'assets/{model_filename}')
-    return aclp
+class AudioCLIPInference(object):
 
-def obtain_embeddings(aclp, audio, labels):
-    text = [[label] for label in labels]
-    ((audio_features, _, _), _), _ = aclp(audio=audio)
-    ((_, _, text_features), _), _ = aclp(text=text)
-    audio_features = audio_features / torch.linalg.norm(audio_features, dim=-1, keepdim=True)
+    def __init__(self,  model_filename = 'AudioCLIP-Full-Training.pt'):
+        torch.set_grad_enabled(False)
+        self.aclp = AudioCLIP(pretrained=f'assets/{model_filename}')
 
-    scale_audio_text = torch.clamp(aclp.logit_scale_at.exp(), min=1.0, max=100.0)
-    logits_audio_text = scale_audio_text * audio_features @ text_features.T
-    return logits_audio_text
+    def obtain_embeddings(self, audio, labels):
+        text = [[label] for label in labels]
+        ((audio_features, _, _), _), _ = self.aclp(audio=audio)
+        ((_, _, text_features), _), _ = self.aclp(text=text)
+        audio_features = audio_features / torch.linalg.norm(audio_features, dim=-1, keepdim=True)
 
-def preprocess_audio(aclp, input_dir,  SAMPLE_RATE = 44100):
+        scale_audio_text = torch.clamp(self.aclp.logit_scale_at.exp(), min=1.0, max=100.0)
+        logits_audio_text = scale_audio_text * audio_features @ text_features.T
+        return logits_audio_text
 
-    audio_transforms = ToTensor1D()
+    def preprocess_audio(self, input_dir,  SAMPLE_RATE = 44100):
 
-    paths_to_audio = glob.glob(f'{input_dir}/*.wav')
-    audio = list()
-    for path_to_audio in paths_to_audio:
-        track, _ = librosa.load(path_to_audio, sr=SAMPLE_RATE, dtype=np.float32)
+        audio_transforms = ToTensor1D()
 
-        spec = aclp.audio.spectrogram(torch.from_numpy(track.reshape(1, 1, -1)))
-        spec = np.ascontiguousarray(spec.numpy()).view(np.complex64)
-        pow_spec = 10 * np.log10(np.abs(spec) ** 2 + 1e-18).squeeze()
+        paths_to_audio = glob.glob(f'{input_dir}/*.wav')
+        audio = list()
+        for path_to_audio in paths_to_audio:
+            track, _ = librosa.load(path_to_audio, sr=SAMPLE_RATE, dtype=np.float32)
 
-        audio.append((track, pow_spec))
+            spec = self.aclp.audio.spectrogram(torch.from_numpy(track.reshape(1, 1, -1)))
+            spec = np.ascontiguousarray(spec.numpy()).view(np.complex64)
+            pow_spec = 10 * np.log10(np.abs(spec) ** 2 + 1e-18).squeeze()
 
+            audio.append((track, pow_spec))
 
-    audio = torch.stack([audio_transforms(track.reshape(1, -1)) for track, _ in audio])
-    return audio, paths_to_audio
+        audio = torch.stack([audio_transforms(track.reshape(1, -1)) for track, _ in audio])
+        return audio, paths_to_audio
 
-def score_inputs(logits_audio_text, paths_to_audio):
-    print('\t\tFilename, Audio\t\t\tTextual Label (Confidence)', end='\n\n')
-    confidence = logits_audio_text.softmax(dim=1)
-    for audio_idx in range(len(paths_to_audio)):
-        # acquire Top-3 most similar results
-        conf_values, ids = confidence[audio_idx].topk(3)
+    @staticmethod
+    def score_inputs(logits_audio_text, paths_to_audio):
+        print('\t\tFilename, Audio\t\t\tTextual Label (Confidence)', end='\n\n')
+        confidence = logits_audio_text.softmax(dim=1)
+        for audio_idx in range(len(paths_to_audio)):
+            # acquire Top-3 most similar results
+            conf_values, ids = confidence[audio_idx].topk(3)
 
-        # format output strings
-        query = f'{os.path.basename(paths_to_audio[audio_idx]):>30s} ->\t\t'
-        results = ', '.join([f'{LABELS[i]:>15s} ({v:06.2%})' for v, i in zip(conf_values, ids)])
+            # format output strings
+            query = f'{os.path.basename(paths_to_audio[audio_idx]):>30s} ->\t\t'
+            results = ', '.join([f'{LABELS[i]:>15s} ({v:06.2%})' for v, i in zip(conf_values, ids)])
 
-        print(query + results)
+            print(query + results)
+
+    def __call__(self, input_dir, labels):
+        audio, paths_to_audio = self.preprocess_audio('demo/audio')
+        logits_audio_text = self.obtain_embeddings(audio, LABELS)
+        self.score_inputs(logits_audio_text, paths_to_audio)
 
 
 if __name__ == '__main__':
 
-    torch.set_grad_enabled(False)
-
-
-
-
+    self = AudioCLIPInference()
+    input_dir = 'demo/audio'
     LABELS = ['dog', 'lightning', 'sneezing', 'alarm clock', 'car horn']
-
-    aclp = load_aclp()
-    audio, paths_to_audio = preprocess_audio(aclp, 'demo/audio')
-    logits_audio_text = obtain_embeddings(aclp, audio, LABELS)
-    score_inputs(logits_audio_text, paths_to_audio)
+    self(input_dir,LABELS)
 
