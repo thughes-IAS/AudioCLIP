2cd5b5a025cf46bdee32f73d51b2a3bf5fda19d8
Tyler Hughes <thughes@integralads.com>
2023-03-29 01:21:32 +0000

    ML-1319. Changes: M  baseline/evaluate.py M  devel

--- baseline/evaluate.py
+++ baseline/evaluate.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sys
+from sklearn import metrics
 
 preds=pd.read_csv(sys.argv[1],sep=',',header=None,names=['filename','category'])
 preds['filename']=preds.filename.str.replace('.wav00000','')
@@ -21,3 +22,8 @@ print(f'Accuracy method 1: {accuracy1:.2f}%')
 counts = combined.match.value_counts()
 accuracy2=100*(counts[1]*50+counts[0]*48)/(50*counts.sum())
 print(f'Accuracy method 2: {accuracy2:.2f}%')
+
+acc =  metrics.accuracy_score(combined.category,combined.pred)
+balanced_acc =  metrics.balanced_accuracy_score(combined.category,combined.pred)
+print(f'Accuracy: {acc:.2f}')
+print(f'Balanced accuracy: {balanced_acc:.2f}')
